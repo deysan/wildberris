b76796499af679d52ed0102817b52496feaa312a
deysan <ideysan@gmail.com>
2021-11-25 11:27:56 +0200

    Get data from firebase

---
+++ js/getGoods.js
@@ -0,0 +1,20 @@
+const getGoods = () => {
+  const links = document.querySelectorAll('.navigation-link');
+
+  const getData = () => {
+    fetch('https://willberries-775aa-default-rtdb.europe-west1.firebasedatabase.app/db.json')
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('goods', JSON.stringify(data))
+      })
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      getData();
+    })
+  })
+}
+
+getGoods();
