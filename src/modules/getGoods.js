f626c3a088f38326b13a4ce91e3507cb4c4f59b8
deysan <ideysan@gmail.com>
2021-11-25 19:08:10 +0200

    Add link to more

--- src/modules/getGoods.js
+++ src/modules/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link');
+  const more = document.querySelector('.more');
 
   const renderGoods = (goods) => {
     const goodsList = document.querySelector('.long-goods-list');
@@ -73,6 +74,13 @@ const getGoods = () => {
 
   if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
     renderGoods(JSON.parse(localStorage.getItem('goods')));
+  }
+
+  if (more) {
+    more.addEventListener('click', (event) => {
+      event.preventDefault();
+      getData();
+    })
   }
 }
 
