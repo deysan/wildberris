7b9c038bc266722acb2738da85f1dec57d42b413
deysan <ideysan@gmail.com>
2021-11-26 21:43:48 +0200

    Fix link

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,8 @@ module.exports = {
       patterns: [
         { from: 'css', to: 'css' },
         { from: 'img', to: 'img' },
+        { from: 'db', to: 'db' },
+        { from: 'db/img', to: 'img' },
         { from: 'favicon', to: 'favicon' },
         { from: 'index.html' },
         { from: 'goods.html' }
